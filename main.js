2dcb4ba22a6b9631ad3c0d84b75551c8670d3022
BSteephenson <benjaminsteephenson@gmail.com>
2014-10-28 18:58:38 -0400

    entry point of application

---
+++ main.js
@@ -0,0 +1,19 @@
+var package_manager = require('./package_manager')
+var scaffolder = require('./scaffolder')
+
+var argv = require('minimist')(process.argv.slice(2));
+
+console.log(process.argv)
+
+if('install' in argv){
+	var name = argv.install
+	var git_repo = argv._[0]
+	package_manager.install(name, git_repo)
+}
+else{
+	var file = argv._[0]
+	argv._.shift()
+	var commands = argv._
+	
+	scaffolder(file, commands)
+}
